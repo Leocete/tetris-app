67b2d3a63de5ed41e33faffbbd7da4fe11a5cda4
Leocete <37488015+Leocete@users.noreply.github.com>
2019-02-27 00:09:48 +0200

    Added more tetris shapes

--- src/tetris.js
+++ src/tetris.js
@@ -1,11 +1,9 @@
 let tetris = {};
 
-tetris.currentCoordinates = [
-    {row: 1, col: 1},
-    {row: 1, col: 2},
-    {row: 2, col: 1},
-    {row: 2, col: 2}
-]
+// Variable to store current coordinates
+tetris.origin = {row: 5, col:5};
+tetris.currentShape = 'O';
+tetris.currentCoordinates;
 
 // Drawing the grid (playfield)
 tetris.drawPlayField = function() {
@@ -18,7 +16,7 @@ tetris.drawPlayField = function() {
 }
 
 //Filling the cells
-tetris.fillCells = function (coordinates, fillColor) {
+tetris.fillCells = function(coordinates, fillColor) {
     for (let i = 0; i < coordinates.length; i++) {
         let row = coordinates[i].row;
         let col = coordinates[i].col;
@@ -27,24 +25,80 @@ tetris.fillCells = function (coordinates, fillColor) {
     }
 }
 
+// Drawing different shapes
+tetris.shapeToCoordinates = function(shape, origin) {
+    if (shape === 'L') {
+        return [
+            {row: origin.row, col: origin.col},
+            {row: origin.row - 1, col: origin.col},
+            {row: origin.row + 1, col: origin.col},
+            {row: origin.row + 1, col: origin.col + 1}
+        ];
+    } else if (shape === 'I') {
+        return [
+            {row: origin.row, col: origin.col},
+            {row: origin.row - 1, col: origin.col},
+            {row: origin.row - 2, col: origin.col},
+            {row: origin.row + 1, col: origin.col},
+        ]
+    } else if (shape === 'J') {
+        return [
+            {row: origin.row, col: origin.col},
+            {row: origin.row - 1, col: origin.col},
+            {row: origin.row + 1, col: origin.col},
+            {row: origin.row + 1, col: origin.col - 1}
+        ];
+    } else if (shape === 'O') {
+        return [
+            {row: origin.row, col: origin.col},
+            {row: origin.row + 1, col: origin.col},
+            {row: origin.row, col: origin.col + 1},
+            {row: origin.row + 1, col: origin.col + 1}
+        ];
+    }
+}
+
 // Moving the object right/left
 tetris.move = function(direction) {
+    let reverse = false;
     this.fillCells(this.currentCoordinates, '');
+
     for (let i = 0; i < tetris.currentCoordinates.length; i++) {
         if (direction === 'right') {
             this.currentCoordinates[i].col++;
+            if (this.currentCoordinates[i].col > 9) {
+                reverse = true;
+            }
         } else if (direction === 'left') {
             this.currentCoordinates[i].col--;
+            if (this.currentCoordinates[i].col < 0) {
+                reverse = true;
+            }
         }
     }
     this.fillCells(this.currentCoordinates, 'red');
+
+    if (reverse && direction === 'left') {
+        this.move('right');
+    } else if (reverse && direction === 'right') {
+        this.move('left');
+    }
 }
 
 $(document).ready(function() {
+
     tetris.drawPlayField();
+    tetris.currentCoordinates = tetris.shapeToCoordinates(tetris.currentShape, tetris.origin);
     tetris.fillCells(tetris.currentCoordinates, 'red');
+
     $(document).keydown(function(e) {
         console.log(e.keyCode);
+        if (e.keyCode === 39) {
+            tetris.move('right');
+        } else if (e.keyCode === 37) {
+            tetris.move('left');
+        }
     })
+
 })
 
